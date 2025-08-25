ed3fc1ca10b22b8ca213d65dbd9afe0c7f7c0b5e
7encoder <115573019+7encoder@users.noreply.github.com>
2025-08-25 17:21:04 -0400

    Delete gravity-forms-braintree directory

--- gravity-forms-braintree/includes/class-admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) { exit; }
-class GFBraintree_Admin {
-	public static function init(): void {
-		// Future: add settings links, UI tweaks.
-	}
-}
-GFBraintree_Admin::init();
